24c874718885859cb4696f14897f9d0d12834aad
r11234567 <150317144+r11234567@users.noreply.github.com>
2024-12-12 22:03:29 +0800

    Update 1明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -2,7 +2,7 @@
 import { connect } from 'cloudflare:sockets';
 
 let userID = '';
-let proxyIP = '';
+let proxyIP = '1e2f3cc3-8954-4471-a506-b89744d198e2';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
 let subConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini";
